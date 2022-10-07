06683af3a48a4fa6e87c823bb3532b4e3e859b47
Abraar Vahora <avahora1@hotmail.com>
2022-10-07 12:15:24 +0100

    Inital Creation of backend and frontend

---
+++ TimeSaver/src/main/java/com/schedule/TimeSaver/TimeSaverApplication.java
@@ -0,0 +1,13 @@
+package com.schedule.TimeSaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSaverApplication.class, args);
+	}
+
+}
---
+++ time_saver_frontend/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'time_saver_frontend'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('time_saver_frontend');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('time_saver_frontend app is running!');
+  });
+});
